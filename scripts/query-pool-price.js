3efd1b6d18471531b5e9508922aa18bf1dd0f859
FidelVe <43641727+FidelVe@users.noreply.github.com>
2024-09-13 22:57:13 -0400

    added script to query pool price

--- scripts/query-pool-price.js
+++ scripts/query-pool-price.js
@@ -2,7 +2,7 @@ const { getPoolsStat } = require("../utils/utils");
 
 async function main() {
   try {
-    const poolId = "0x2"; // Pool ID for sICX/bnUSD
+    const poolId = "0x47"; // Pool ID for sICX/bnUSD
     const response = await getPoolsStat(poolId);
     console.log("Response:");
     console.log(response);
