0d1d84eee47f4424f155c562d610bb3537302148
FidelVe <43641727+FidelVe@users.noreply.github.com>
2024-09-18 15:54:19 -0400

    refactored script

--- scripts/make-evm-evm-swap-2.js
+++ scripts/make-evm-evm-swap-2.js
@@ -4,27 +4,32 @@
 // Chain to AVAX (native) on Avalanche Chain.
 //
 const config = require("../utils/config");
-const { Web3 } = require("web3");
+const { ethers } = require("ethers");
 const {
   getRlpEncodedSwapData,
   getPoolsStat,
   depositNativeEvm,
   getContractObjectEvm,
 } = require("../utils/utils");
-const { ethers } = require("ethers");
+
+const EVM_PROVIDER = new ethers.JsonRpcProvider(
+  config.network.evm3[config.useNetwork].rpc,
+);
+const EVM_SIGNER = new ethers.Wallet("0x" + config.privateKey, EVM_PROVIDER);
 
 async function main() {
   try {
     // get abi and address for xcall contract
     const xcallAbi = config.abi.xcall;
-    const xcallAddress = config.network.evm1[config.useNetwork].contracts.xcall;
+    const xcallAddress = config.network.evm3[config.useNetwork].contracts.xcall;
     const xcallManagerAbi = config.abi.xcallManager;
     const xcallManagerAddress =
-      config.network.evm1[config.useNetwork].contracts.xcall_manager;
+      config.network.evm3[config.useNetwork].contracts.xcall_manager;
 
     const xcallManagerContract = getContractObjectEvm(
       xcallManagerAbi,
       xcallManagerAddress,
+      EVM_SIGNER,
     );
 
     // get the sources for the cross chain transfer
@@ -32,11 +37,15 @@ async function main() {
     const sources = JSON.parse(JSON.stringify(responseProtocols))[0];
 
     // instantiate the contract object
-    const xcallContract = getContractObjectEvm(xcallAbi, xcallAddress);
+    const xcallContract = getContractObjectEvm(
+      xcallAbi,
+      xcallAddress,
+      EVM_SIGNER,
+    );
 
     // get fee for cross chain transfer using xcall
     const response = await xcallContract["getFee(string,bool,string[])"](
-      config.network.evm3[config.useNetwork].xnid,
+      config.network.evm2[config.useNetwork].xnid,
       "0x1",
       sources,
     );
@@ -44,18 +53,12 @@ async function main() {
     if (Number.isNaN(parsedFee)) {
       throw new Error("Failed to parse fee");
     }
-    // instantiate providers
-    const evmProvider = new Web3(config.network.evm1[config.useNetwork].rpc);
-
-    const evmWallet = evmProvider.eth.accounts.privateKeyToAccount(
-      "0x" + config.privateKey,
-    );
-    const receiver = `${config.network.evm3[config.useNetwork].xnid}/${evmWallet.address}`;
+    const receiver = `${config.network.evm2[config.useNetwork].xnid}/${EVM_SIGNER.address}`;
 
     // amount to trade
-    const amountRaw = 0.1;
+    const amountRaw = 0.0017;
     let amount = amountRaw + parsedFee;
-    amount = Number(amount.toFixed(4));
+    amount = Number(amount.toFixed(6));
 
     const parsedAmountRaw = parseInt(amountRaw * 10 ** 18);
     const parsedAmount = parseInt(amount * 10 ** 18);
@@ -63,10 +66,10 @@ async function main() {
     // slippage of 5%
     const slippage = 0.02;
 
-    // fetch the current price for the AVAX/bnUSD pool
+    // fetch the current price for the BNB/bnUSD pool
     // and ETH/bnUSD pool
-    const pool1Data = await getPoolsStat("0x46");
-    const pool2Data = await getPoolsStat("0x3b");
+    const pool1Data = await getPoolsStat("0x3b");
+    const pool2Data = await getPoolsStat("0x47");
     const pool1Price = parseInt(pool1Data.result.price, 16) / 10 ** 18;
     const pool2Price = parseInt(pool2Data.result.price, 16) / 10 ** 18;
 
@@ -90,11 +93,19 @@ async function main() {
     console.log(encodedData);
     const routerReceiver = `${config.network.icon[config.useNetwork].xnid}/${config.network.icon[config.useNetwork].contracts.router}`;
 
+    const assetManagerContract = getContractObjectEvm(
+      config.abi.assetManager,
+      config.network.evm3[config.useNetwork].contracts.asset_manager,
+      EVM_SIGNER,
+    );
     const hash = await depositNativeEvm(
       "0x" + parsedAmountRaw.toString(16),
       routerReceiver,
       "0x" + encodedData,
       "0x" + parsedAmount.toString(16),
+      false,
+      assetManagerContract,
+      { gasLimit: 25000 },
     );
 
     console.log("Transaction result:");
